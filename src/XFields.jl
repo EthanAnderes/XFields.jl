485976e6da805ae34fcbf01551e17411219b6b78
Ethan Anderes <anderes@ucdavis.edu>
2020-05-08 09:20:02 -0700

    export diag method for DiagOp

--- src/XFields.jl
+++ src/XFields.jl
@@ -43,7 +43,9 @@ include("field_methods.jl")
 
 include("linear_ops.jl")
 
-export Field, FourierField, MapField, DiagOp, AbstractLinearOp, fielddata, fieldtransform
+export Field, FourierField, MapField, fielddata, fieldtransform
+export DiagOp, AbstractLinearOp, diag 
+#TODO: add adjoint method for DiagOp
 
 # Specific implimentation 
 # =========================================
