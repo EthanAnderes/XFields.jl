39925a96c70c8313c47840ba914545911e8b8583
Ethan Anderes <anderes@ucdavis.edu>
2020-07-27 17:10:47 -0700

    make AbstractLinearOp actually useful by generically defining operator chaining

--- src/linear_ops.jl
+++ src/linear_ops.jl
@@ -1,29 +1,145 @@
-# linear operators diagonal in a Field basis
+# linear operators on fields
 # ============================================================
 
-abstract type AbstractLinearOp{X<:Field} end
+"""
+Interface requirements for AbstractLinearOp
+```
+Base.:inv(O::AbstractLinearOp) 
+LinearAlgebra.adjoint(O::AbstractLinearOp) 
+Base.:*(O::AbstractLinearOp, f::Field) 
+Base.:\\(O::AbstractLinearOp, f::Field)
+```
+Optionally 
+```
+_lmult(O::AbstractLinearOp, f::Field) = O * f
+```
+can be specialized to avoid a final conversion back to 
+the argument field type as `*` would typically do. 
+Useful for a sequence of operations that avoid un-necessary conversions.
+"""
+abstract type AbstractLinearOp end
 
-struct DiagOp{X<:Field} <: AbstractLinearOp{X}
+
+_lmult(O::AbstractLinearOp, f) = O * f
+
+
+# Chains of AbstractLinearOp
+# ------------------------------------------
+
+# `*` concatinates chains
+function Base.:*(O1::AbstractLinearOp, O2::AbstractLinearOp)
+	tuple(O1, O2)
+end
+
+function Base.:*(O1::NTuple{N,AbstractLinearOp}, O2::AbstractLinearOp) where N 
+	Base.front(O1) * (Base.last(O1) * O2)
+end
+
+function Base.:*(O1::AbstractLinearOp, O2::NTuple{N,AbstractLinearOp}) where N
+	(O1 * Base.first(O2)) * Base.tail(O2)
+end
+
+function Base.:*(O1::NTuple{N,AbstractLinearOp}, O2::NTuple{M,AbstractLinearOp}) where {N,M}
+	tuple(O1..., O2...)
+end
+
+# `inv` broadcasts and reverses order
+function Base.:inv(O1::NTuple{N,AbstractLinearOp}) where N
+	tuple((inv(op) for op in reverse(O1))...)
+end
+
+# `adjoint` broadcasts and reverses order
+function Base.:adjoint(O1::NTuple{N,AbstractLinearOp}) where N
+	tuple((adjoint(op) for op in reverse(O1))...)
+end
+
+# `\`
+function Base.:\(O1::AbstractLinearOp, O2::AbstractLinearOp)
+	inv(O1) * O2
+end
+
+function Base.:\(O1::NTuple{N,AbstractLinearOp}, O2::AbstractLinearOp) where N
+	inv(O1) * O2
+end
+
+function Base.:\(O1::AbstractLinearOp, O2::NTuple{N,AbstractLinearOp}) where N
+	inv(O1) * O2
+end
+
+function Base.:\(O1::NTuple{N,AbstractLinearOp}, O2::NTuple{M,AbstractLinearOp}) where {N,M}
+	inv(O1) * O2
+end
+
+# `/`
+function Base.:/(O1::AbstractLinearOp, O2::AbstractLinearOp) 
+	O1 * inv(O2) 
+end
+
+function Base.:/(O1::NTuple{N,AbstractLinearOp}, O2::AbstractLinearOp) where N
+	O1 * inv(O2)
+end
+
+function Base.:/(O1::AbstractLinearOp, O2::NTuple{N,AbstractLinearOp}) where N
+	O1 * inv(O2)
+end
+
+function Base.:/(O1::NTuple{N,AbstractLinearOp}, O2::NTuple{M,AbstractLinearOp}) where {N,M}
+	O1 * inv(O2)
+end
+
+# activate the lazy tuple when operating
+function Base.:*(O1::NTuple{N,AbstractLinearOp}, f::Y) where {N,Y<:Field}
+	Y(foldr(_lmult, (O1..., f)))
+end
+
+function Base.:\(O1::NTuple{N,AbstractLinearOp}, f::Y) where {N,Y<:Field}
+	(inv(O1) * f)::Y
+end 
+
+
+
+
+# DiagOp <: AbstractLinearOp
+# ======================================
+
+# Most of the behavior of DiagOps is defined 
+# using field ops ... in analogy to f(UΛU⁻¹) = U f(Λ) U⁻¹ 
+# where f(Λ) ≡ Diagonal(f.(diag(Λ))) 
+
+struct DiagOp{X<:Field} <: AbstractLinearOp
     f::X
 end
 
-# Most of the behavior of DiagOps is defined 
-# using field ops ... in analogy to foo(UΛU⁻¹) = U foo(Λ) Uᵀ 
-# where foo(Λ) can be considered an operation of the field diag(Λ)
+# Interfact methods
+# ---------------------------------------
+
+function Base.:inv(O::DiagOp{X}) where X<:Field 
+	## DiagOp(X(fieldtransform(O.f), nan2zero.(inv.(fielddata(O.f)))))
+	DiagOp(X(fieldtransform(O.f), pinv.(fielddata(O.f))))
+end
+
+function LinearAlgebra.adjoint(O::DiagOp{X}) where X<:Field 
+	DiagOp(X(fieldtransform(O.f), conj.(fielddata(O.f))))
+end
+
+function Base.:*(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}} 
+	Y(O.f * X(f))
+end
+
+function Base.:\(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}} 
+	Y(O.f \ X(f))
+end
+
+_lmult(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}} = O.f * X(f)
 
 # getindex and basic operator functionality
 # ------------------------------------------
 
 LinearAlgebra.diag(O::DiagOp) = O.f
-Base.getindex(O::DiagOp, i)   = getindex(O.f, i) # indexing is propigated
-Base.:*(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}} = Y(O.f * X(f))
-Base.:\(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}} = Y(O.f \ X(f))
 
-# The * method above does a final conversion to the original field type. 
-# Here is a version that doesn't do the final conversion. 
-# Useful for a sequence of operations that avoid un-necessary conversions
-_lmult(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}} = O.f * X(f)
-_ldiv(O::DiagOp{X}, f::Y) where {F<:Transform, X<:Field{F}, Y<:Field{F}}  = O.f \ X(f)
+function Base.getindex(O::DiagOp, i)  
+	getindex(O.f, i) # indexing is propigated
+end
 
 # Operations op(DiagOp), op(DiagOp, Number) and  op(Number, DiagOp)
 # ------------------------------------------
@@ -45,39 +161,7 @@ Base.:^(O::DiagOp{X}, a::Integer) where X<:Field = DiagOp(O.f^a)
 
 # op(DiagOp)
 Base.:sqrt(O::DiagOp{X}) where X<:Field = DiagOp(sqrt(O.f))
-Base.:inv(O::DiagOp{X})  where X<:Field = DiagOp(X(fieldtransform(O.f), nan2zero.(inv.(fielddata(O.f)))))
 
-# Operations op(DiagOp, DiagOp
-# ------------------------------------------
-
-# ops of the same type
-Base.:+(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f + O2.f)
-Base.:-(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f - O2.f)
-Base.:*(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f * O2.f)
-Base.:\(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f \ O2.f)
-Base.:/(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f / O2.f)
-
-# chains of linear ops that are not of the same type store a lazy tuple
-Base.:*(O1::DiagOp, O2::DiagOp) = tuple(O1, O2)
-Base.:*(O1::NTuple{N,DiagOp}, O2::DiagOp) where N = Base.front(O1) * (Base.last(O1) * O2)
-Base.:*(O1::DiagOp, O2::NTuple{N,DiagOp}) where N = (O1 * Base.first(O2)) * Base.tail(O2)
-Base.:*(O1::NTuple{N,DiagOp}, O2::NTuple{M,DiagOp}) where {N,M} = tuple(O1..., O2...)
-
-Base.:inv(O1::NTuple{N,DiagOp}) where N = tuple((inv(op) for op in reverse(O1))...)
-
-Base.:\(O1::DiagOp, O2::DiagOp)                   = inv(O1) * O2
-Base.:\(O1::NTuple{N,DiagOp}, O2::DiagOp) where N = inv(O1) * O2
-Base.:\(O1::DiagOp, O2::NTuple{N,DiagOp}) where N = inv(O1) * O2
-Base.:\(O1::NTuple{N,DiagOp}, O2::NTuple{M,DiagOp}) where {N,M} = inv(O1) * O2
-
-Base.:/(O1::DiagOp, O2::DiagOp)                   = O1 * inv(O2) 
-Base.:/(O1::NTuple{N,DiagOp}, O2::DiagOp) where N = O1 * inv(O2)
-Base.:/(O1::DiagOp, O2::NTuple{N,DiagOp}) where N = O1 * inv(O2)
-Base.:/(O1::NTuple{N,DiagOp}, O2::NTuple{M,DiagOp}) where {N,M} = O1 * inv(O2)
-
-# activate the lazy tuple when operating
-Base.:*(O1::NTuple{N,DiagOp}, f::Y) where {N,Y<:Field} = Y(foldr(_lmult, (O1..., f)))
-Base.:\(O1::NTuple{N,DiagOp}, f::Y) where {N,Y<:Field} = (inv(O1) * f)::Y 
 
 # Operations between DiagOp and UniformScaling
 # ------------------------------------------
@@ -90,7 +174,43 @@ LinearAlgebra.:-(O::DiagOp, J::UniformScaling) = DiagOp(O.f - J.λ)
 LinearAlgebra.:-(J::UniformScaling, O::DiagOp) = DiagOp(J.λ - O.f)
 
 
+# Operations op(DiagOp, DiagOp)
+# ------------------------------------------
 
+# ops of the same type
+Base.:+(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f + O2.f)
+Base.:-(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f - O2.f)
+Base.:*(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f * O2.f)
+Base.:\(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f \ O2.f)
+Base.:/(O1::DiagOp{X}, O2::DiagOp{X}) where X<:Field = DiagOp(O1.f / O2.f)
+
+
+
+## # Operations op(DiagOp, DiagOp
+## # ------------------------------------------
+## 
+## # chains of linear ops that are not of the same type store a lazy tuple
+## Base.:*(O1::DiagOp, O2::DiagOp) = tuple(O1, O2)
+## Base.:*(O1::NTuple{N,DiagOp}, O2::DiagOp) where N = Base.front(O1) * (Base.last(O1) * O2)
+## Base.:*(O1::DiagOp, O2::NTuple{N,DiagOp}) where N = (O1 * Base.first(O2)) * Base.tail(O2)
+## Base.:*(O1::NTuple{N,DiagOp}, O2::NTuple{M,DiagOp}) where {N,M} = tuple(O1..., O2...)
+## 
+## Base.:inv(O1::NTuple{N,DiagOp}) where N = tuple((inv(op) for op in reverse(O1))...)
+## 
+## Base.:\(O1::DiagOp, O2::DiagOp)                   = inv(O1) * O2
+## Base.:\(O1::NTuple{N,DiagOp}, O2::DiagOp) where N = inv(O1) * O2
+## Base.:\(O1::DiagOp, O2::NTuple{N,DiagOp}) where N = inv(O1) * O2
+## Base.:\(O1::NTuple{N,DiagOp}, O2::NTuple{M,DiagOp}) where {N,M} = inv(O1) * O2
+## 
+## Base.:/(O1::DiagOp, O2::DiagOp)                   = O1 * inv(O2) 
+## Base.:/(O1::NTuple{N,DiagOp}, O2::DiagOp) where N = O1 * inv(O2)
+## Base.:/(O1::DiagOp, O2::NTuple{N,DiagOp}) where N = O1 * inv(O2)
+## Base.:/(O1::NTuple{N,DiagOp}, O2::NTuple{M,DiagOp}) where {N,M} = O1 * inv(O2)
+## 
+## # activate the lazy tuple when operating
+## Base.:*(O1::NTuple{N,DiagOp}, f::Y) where {N,Y<:Field} = Y(foldr(_lmult, (O1..., f)))
+## Base.:\(O1::NTuple{N,DiagOp}, f::Y) where {N,Y<:Field} = (inv(O1) * f)::Y 
+## 
 
 
 
